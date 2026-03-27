f46b6f6d10760deeee790d260b0ec54822f9af8b
AfnanAfa <aahrafafna2368@gmail.com>
2026-03-27 14:42:57 +0530

    27/3/2026

---
+++ paliandrom.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+
+echo "Enter a string:"
+read str
+
+rev=""
+
+len=${#str}
+
+for (( i=$len-1; i>=0; i-- ))
+do
+    rev="$rev${str:$i:1}"
+done
+
+if [ "$str" = "$rev" ]; then
+    echo "Palindrome"
+else
+    echo "Not a Palindrome"
+fi
